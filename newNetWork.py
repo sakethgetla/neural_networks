25815a7b55af42c22a3a9da975e406610ada67cd
sakethgetla <saketh.getla@gmail.com>
2018-10-20 20:24:53 +1100

    efesf

--- newNetWork.py
+++ newNetWork.py
@@ -78,6 +78,8 @@ def calculateOutput(input,w,b):
     #b #bais
     #input #training set 2d vector
 
+    print("b " + str(b))
+    
     print("weight ")
     print(w)
 
@@ -95,8 +97,6 @@ def calculateOutput(input,w,b):
     print("a = np.multiply(np.identity(2),input)")
     a = np.multiply(np.identity(2),input)
     print(a)
-    
-
 
     identy = np.identity(2,None)
     print("identy = np.identity(2,None)")
@@ -105,27 +105,55 @@ def calculateOutput(input,w,b):
     print("identyWeight = np.multiply(identy,w)")
     print(identyWeight)
     print("")
-    print("return  np.multiply(identyWeight,input)")
-    return  np.multiply(identyWeight,input)
+    
+    print("np.multiply(identyWeight,input)")
+    multiply = np.multiply(identyWeight,input)
+    print(multiply)
+    print("creating row")
+    print("np.multiply(multiply,np.ones(2))")
+    print(np.multiply(multiply,np.ones(2)))
+
+    print(multiply)
+    print(multiply[0])
+    print(multiply[1])
+    sa = np.vectorize(multiply)
+    print("np.diag(multiply)")
+    multiply = np.diag(multiply)
+    print(multiply)
+    
+    print("addBias = np.add(multiply,b)")
+    addBias = np.add(multiply,b)
+    print(addBias)
+    print(np.transpose( addBias))
+    print("ddddddddddd")
+    print( np.reshape( addBias,(-1,1)))
+#    matxOutputs[:,0] =  addBias
+ #   print("matrix output" + str(matxOutputs))
+    return   addBias
+
+#    matxOutputs[:,0:1] = np.reshape( addBias,(-1,1))
+#    print(matxOutputs)
+
+
     #matxOutputs[:,0:1] = np.multiply(identy,input)
-    
-    
+        
 
-def sigmoid(self, input):
-    self.output = 1/(1+math.exp(-input))
-    print("sigmoid output " + str(self.output))
+def sigmoid(input):
+    output = 1/(1+math.exp(-input))
+    print("sigmoid output " + str(output))
 
 
-def update(self, gameDisplay, color):
+def update(gameDisplay, color):
     myfont = pygame.font.SysFont("monospace", 15)
     output = self.output
     label = myfont.render(str(output), 1, (0, 255, 255))
     gameDisplay.blit(label, self.XYpos)
-    pygame.draw.circle(gameDisplay, color, self.XYpos, 20, 3)
+    pygame.draw.circle(gamedisplay, color, self.XYpos, 20, 3)
 
 
 def gameLoop():
     gameDisplay.fill(gray)
+    currPlace = 0
     gameExit = False
     while not gameExit:
         pygame.display.update()
@@ -133,8 +161,49 @@ def gameLoop():
             if event.type == pygame.QUIT:
                 gameExit = True
         clock.tick(FPS)
+        key = pygame.key.get_pressed()
+        if key[pygame.K_d]:
+            print("corrpos " + str(currPlace))
+            if(currPlace % 4 == 3):
+                currPlace += 1
+                calResult(training[0][0])
+            elif(currPlace % 4 == 2):
+                currPlace += 1
+                calResult(training[1][0]) 
+            elif(currPlace % 4 == 1):
+                currPlace += 1
+                calResult(training[2][0])
+            elif(currPlace % 4 == 0):
+                currPlace += 1
+                calResult(training[3][0])
     pygame.quit()
     quit()
 
-print(calculateOutput([[2,3]],matxWeights[:,0:1],vecBais))
+
+#i=0
+#print("train")
+#print(training[0][i])
+#print("input weights")
+#print(matxWeights[:,i:1])
+#calculateOutput([training[0][i]],matxWeights[:,i:1],vecBais[i])
+#
+def calResult(train):
+    for p in range(4):
+        print("train")
+        print(train)
+        print("input weights")
+        print(matxWeights[:,0])
+        print(calculateOutput([train],matxWeights[:,0],vecBais[0]))
+        matxOutputs[:,0] = calculateOutput([train],matxWeights[:,0], vecBais[0])
+        print("matrix output" + str(matxOutputs))
+        matxOutputs[:,1] = calculateOutput([matxOutputs[:,0]],matxWeights[:,1],vecBais[1])
+        print("matrix output" + str(matxOutputs))                                   
+        matxOutputs[:,2] = calculateOutput([matxOutputs[:,1]],matxWeights[:,2],vecBais[2])
+        print(matxWeights)
+        print("matrix output")
+        print(matxOutputs)
+
+
+
+
 gameLoop()
