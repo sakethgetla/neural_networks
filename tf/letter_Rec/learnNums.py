66db3d52082fb18edfa3099eb5a1795260c13e4b
sakethgetla <saketh.getla@gmail.com>
2020-01-15 15:09:08 +1100

    autopushed

---
+++ tf/letter_Rec/learnNums.py
@@ -0,0 +1,141 @@
+import pandas as pd     # data frame manipleation
+#import seaborn as sns   # perform auto plot images stat plots adv matplot lib
+import numpy as np      # numerical analysis
+import math
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+
+from tensorflow.keras.layers import Dense, InputLayer, Flatten
+#from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras import Model
+from pdb import set_trace as bp
+
+#data_df = pd.read_csv('mnist.csv')
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+#bp()
+# Add a channels dimension
+#x_train = x_train[..., tf.newaxis]
+#x_test = x_test[..., tf.newaxis]
+
+train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(100000).batch(50)
+
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(50)
+
+class MyModel(Model):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        #self.input = InputLayer(input_tensor=np.shape(x_train[0]))
+        #self.inp= InputLayer()
+        #self.inp= InputLayer(input_tensor=x_train[0])
+        #self.d1 = Dense(5, activation='sigmoid')
+        self.flatten = Flatten()
+        self.d1 = Dense(100, activation='tanh')
+        #self.d1 = Dense(15, activation='tanh', input_shape = [28, 28, 1])
+        #self.d1 = Dense(10,  activation='relu')
+        self.d2 = Dense(20, activation='tanh')
+        #self.d3 = Dense(1, activation='tanh')
+        #self.d3 = Dense(1, activation='sigmoid')
+        #self.d3 = Dense(1, activation='sigmoid', use_bias=False)
+        self.d3 = Dense(1)
+        #self.d4 = Dense(1, activation='relu')
+
+    def call(self, x):
+        #x = self.inp(x)
+        x = self.flatten(x)
+        x = self.d1(x)
+        x = self.d2(x)
+        #x = self.d3(x)
+        #x = self.d4(x)
+        return self.d3(x)
+
+    #def predict(self, x):
+    #    return super(MyModel, self).predict(x)
+
+
+#assert(False == True)
+
+model = MyModel()
+loss_obj = tf.keras.losses.MeanSquaredError()
+optimizer = tf.keras.optimizers.Adam(0.0001)
+
+train_loss = tf.keras.metrics.Mean(name='test_loss')
+train_accuracy = tf.keras.metrics.MeanSquaredError(name='test_accuracy')
+
+test_loss = tf.keras.metrics.Mean(name='test_loss')
+test_accuracy = tf.keras.metrics.MeanSquaredError(name='test_accuracy')
+
+
+
+@tf.function
+def train_step(x, y):
+    with tf.GradientTape() as tape:
+        #print("x = {x} y = {y}")
+        #tf.print(x, output_stream=sys.stdout)
+        #print(f"x = {x} y = {y}")
+        predictions = model(x)
+        loss = loss_obj(y, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+    train_loss(loss)
+    train_accuracy(y, predictions)
+
+
+@tf.function
+def test_step(x, y):
+    predictions = model(x)
+    t_loss = loss_obj(y, predictions)
+
+    test_loss(t_loss)
+    test_accuracy(y, predictions)
+
+
+
+
+EPOCHS = 100
+model_hist = []
+
+for epoch in range(EPOCHS):
+    # Reset the metrics at the start of the next epoch
+    train_loss.reset_states()
+    train_accuracy.reset_states()
+    test_loss.reset_states()
+    test_accuracy.reset_states()
+
+    #for x, y in zip(x_train, y_train):
+    #bp()
+    model.predict(x_train[:1])
+    for x, y in train_ds:
+        train_step(x, y)
+
+    #for x, y in zip(x_test, y_test):
+    for x, y in test_ds:
+        test_step(x, y)
+    model_hist.append(train_loss.result())
+
+    template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
+    print(template.format(epoch+1,
+                          train_loss.result(),
+                          train_accuracy.result()*100,
+                          test_loss.result(),
+                          test_accuracy.result()*100))
+
+
+model.save_weights('weights', save_format='tf')
+
+ans = [model.predict(x_test)]
+# cant call predict function here? but can call in before traning
+
+#model.save('sincurveModel.h5')
+colors = 'red'
+area = 7
+plt.plot(model_hist)
+plt.show()
+
+
+
+
---
+++ tf/letter_Rec/templet.py
@@ -0,0 +1,160 @@
+import pygame
+import math
+import numpy as np
+import tensorflow as tf
+from pdb import set_trace as bp
+
+from tensorflow.keras.layers import Dense, InputLayer, Flatten
+#from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras import Model
+
+#from learnNums.py import MyModel
+
+pygame.init()
+gray = (115, 115, 115)
+black = (0, 0, 0)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+green = (0, 255, 0)
+lightGreen = (155, 255, 0)
+yellow = (255, 255, 0)
+white = (255, 255, 255)
+
+display_width = 800
+display_height = 800
+
+pointSize = 50
+
+width = 10
+height = width
+
+clock = pygame.time.Clock()
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+FPS = 30
+ans = np.zeros((1, 28, 28))
+
+#model = MyModel()
+#model.load_weights('weights')
+
+def dist(x1, x2):
+    return math.sqrt((x1[0] - x2[0])**2 + (x1[1] - x2[1])**2 )
+
+class Btn():
+    def __init__(self, pos, size, gridPos):
+        self.pos = pos
+        self.size = size
+        self.click= False
+        self.gridPos= gridPos
+
+    def draw(self, gameDisplay):
+        if(self.click):
+            pygame.draw.rect(gameDisplay, black, [self.pos[0], self.pos[1], self.size[0], self.size[1]])
+        else :
+            pygame.draw.rect(gameDisplay, white, [self.pos[0], self.pos[1], self.size[0], self.size[1]]) 
+
+    def clicked(self, clickPos):
+        if(not self.click):
+            if (dist(clickPos, self.pos) < 10):
+            #if(clickPos[0] < self.pos[0] + self.size[0] and clickPos[0] > self.pos[0]):
+            #    if(clickPos[1] < self.pos[1] + self.size[1] and clickPos[1] > self.pos[1]):
+                ans[0][self.gridPos[0], self.gridPos[1]] = 1
+                self.click = True
+
+
+
+#def btn(gameDisplay, pos, size,  color, txt, action, mouse, click):
+#    global startBtnClicked
+#    #click = pygame.mouse.get_rel()
+#    #print(click)
+#    click = pygame.mouse.get_pressed()
+#
+#    if (click[0] == 1 and action != None ):
+#        if ( pos[0] < mouse[0] < pos[0]+size[0] and not startBtnClicked):
+#            if ( pos[1] < mouse[1] < pos[1]+size[1]):
+#                startBtnClicked = True
+#                action()
+#    else :
+#        startBtnClicked = False
+#
+#    pygame.draw.rect(gameDisplay, color, [pos[0], pos[1], size[0], size[1]] )
+#    myfont = pygame.font.SysFont("monospace", 15)
+#    label = myfont.render(str(txt), 1, black)
+#    gameDisplay.blit(label, pos)
+
+def draw():
+    pass
+
+class MyModel(Model):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        #self.input = InputLayer(input_tensor=np.shape(x_train[0]))
+        #self.inp= InputLayer()
+        #self.inp= InputLayer(input_tensor=x_train[0])
+        #self.d1 = Dense(5, activation='sigmoid')
+        self.flatten = Flatten()
+        self.d1 = Dense(100, activation='tanh')
+        #self.d1 = Dense(15, activation='tanh', input_shape = [28, 28, 1])
+        #self.d1 = Dense(10,  activation='relu')
+        self.d2 = Dense(20, activation='tanh')
+        #self.d3 = Dense(1, activation='tanh')
+        #self.d3 = Dense(1, activation='sigmoid')
+        #self.d3 = Dense(1, activation='sigmoid', use_bias=False)
+        self.d3 = Dense(1)
+        #self.d4 = Dense(1, activation='relu')
+
+    def call(self, x):
+        #x = self.inp(x)
+        x = self.flatten(x)
+        x = self.d1(x)
+        x = self.d2(x)
+        #x = self.d3(x)
+        #x = self.d4(x)
+        return self.d3(x)
+
+
+model = MyModel()
+#bp()
+model.predict(ans)
+model.load_weights('weights')
+
+def gameLoop():
+
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    gameDisplay.fill(gray)
+    gameExit = False
+    global gravityON
+    nodes = []
+    notepad = []
+    notepadSize = 28
+    btnSize = [display_width/(3*notepadSize), display_height/(3*notepadSize)]
+
+    for i in range(notepadSize):
+        for j in range(notepadSize):
+            notepad.append(Btn([int(i*btnSize[0]), int(j*btnSize[1])], btnSize, (i, j)))
+
+    while not gameExit:
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        for tile in notepad:
+            if( click[0] == 1):
+            #if (click[0] == 1 and action != None ):
+                tile.clicked([int(mouse[0]), int(mouse[1])])
+            tile.draw(gameDisplay)
+
+        pygame.display.update()
+        gameDisplay.fill(gray)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
+
+        print(model(ans))
+        clock.tick(FPS)
+        key = pygame.key.get_pressed()
+        if key[pygame.K_q] :
+            gameExit = True
+
+    pygame.quit()
+    quit()
+gameLoop()
