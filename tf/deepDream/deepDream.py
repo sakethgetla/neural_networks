ca97053f64be274bf3722e496d9f1c0fb251fa2a
sakethgetla <saketh.getla@gmail.com>
2020-01-23 12:25:35 +1100

    autopushed

--- tf/deepDream/deepDream.py
+++ tf/deepDream/deepDream.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 import numpy as np
-
 import matplotlib as mpl
+import cv2
 
-import IPython.display as display
-import PIL.Image
+import matplotlib.pyplot as plt
+from pdb import set_trace as bp
+#import IPython.display as display
+#import PIL.Image
 
 from tensorflow.keras.preprocessing import image
 
@@ -19,97 +21,104 @@ dream_model = tf.keras.Model(inputs=base_model.input, outputs=layers)
 
 # Normalize an image
 def deprocess(img):
-  img = 255*(img + 1.0)/2.0
-  return tf.cast(img, tf.uint8)
+    img = 255*(img + 1.0)/2.0
+    return tf.cast(img, tf.uint8)
 
 # Display an image
 def show(img):
-  display.display(PIL.Image.fromarray(np.array(img)))
-#plt.imshow(smoothenedImg, cmap='gray', vmin=0, vmax=1)
+    #display.display(PIL.Image.fromarray(np.array(img)))
+    plt.imshow(img, cmap='gray', vmin=0, vmax=1)
+    plt.show()
 
 
 # Downsizing the image makes it easier to work with.
-original_img = download(url, max_dim=500)
-show(original_img)
+#original_img = download(url, max_dim=500)
+#original_img = 'eye.jpg'
+original_img = cv2.imread('nightsky.jpeg' , cv2.IMREAD_COLOR)    
+#show(original_img)
+#bp()
+
 #display.display(display.HTML('Image cc-by: <a "href=https://commons.wikimedia.org/wiki/File:Felis_catus-cat_on_snow.jpg">Von.grzanka</a>'))
 
 def calc_loss(img, model):
-  # Pass forward the image through the model to retrieve the activations.
-  # Converts the image into a batch of size 1.
-  img_batch = tf.expand_dims(img, axis=0)
-  layer_activations = model(img_batch)
-  if len(layer_activations) == 1:
-    layer_activations = [layer_activations]
+    # Pass forward the image through the model to retrieve the activations.
+    # Converts the image into a batch of size 1.
+    img_batch = tf.expand_dims(img, axis=0)
+    layer_activations = model(img_batch)
+    if len(layer_activations) == 1:
+        layer_activations = [layer_activations]
 
-  losses = []
-  for act in layer_activations:
-    loss = tf.math.reduce_mean(act)
-    losses.append(loss)
+    losses = []
+    for act in layer_activations:
+        loss = tf.math.reduce_mean(act)
+        losses.append(loss)
 
-  return  tf.reduce_sum(losses)
+    return  tf.reduce_sum(losses)
 
 class DeepDream(tf.Module):
-  def __init__(self, model):
-    self.model = model
+    def __init__(self, model):
+        self.model = model
 
-  @tf.function(
-      input_signature=(
+    @tf.function(
+        input_signature=(
         tf.TensorSpec(shape=[None,None,3], dtype=tf.float32),
         tf.TensorSpec(shape=[], dtype=tf.int32),
         tf.TensorSpec(shape=[], dtype=tf.float32),)
-  )
-  def __call__(self, img, steps, step_size):
-      print("Tracing")
-      loss = tf.constant(0.0)
-      for n in tf.range(steps):
-        with tf.GradientTape() as tape:
-          # This needs gradients relative to `img`
-          # `GradientTape` only watches `tf.Variable`s by default
-          tape.watch(img)
-          loss = calc_loss(img, self.model)
+        )
+    def __call__(self, img, steps, step_size):
+        print("Tracing")
+        loss = tf.constant(0.0)
+        for n in tf.range(steps):
+            with tf.GradientTape() as tape:
+                # This needs gradients relative to `img`
+                # `GradientTape` only watches `tf.Variable`s by default
+                tape.watch(img)
+                loss = calc_loss(img, self.model)
 
         # Calculate the gradient of the loss with respect to the pixels of the input image.
-        gradients = tape.gradient(loss, img)
+            gradients = tape.gradient(loss, img)
 
-        # Normalize the gradients.
-        gradients /= tf.math.reduce_std(gradients) + 1e-8 
-        
-        # In gradient ascent, the "loss" is maximized so that the input image increasingly "excites" the layers.
-        # You can update the image by directly adding the gradients (because they're the same shape!)
-        img = img + gradients*step_size
-        img = tf.clip_by_value(img, -1, 1)
+            # Normalize the gradients.
+            gradients /= tf.math.reduce_std(gradients) + 1e-8 
 
-      return loss, img
+            # In gradient ascent, the "loss" is maximized so that the input image increasingly "excites" the layers.
+            # You can update the image by directly adding the gradients (because they're the same shape!)
+            img = img + gradients*step_size
+            img = tf.clip_by_value(img, -1, 1)
+
+        return loss, img
 
 deepdream = DeepDream(dream_model)
 
 def run_deep_dream_simple(img, steps=100, step_size=0.01):
-  # Convert from uint8 to the range expected by the model.
-  img = tf.keras.applications.inception_v3.preprocess_input(img)
-  img = tf.convert_to_tensor(img)
-  step_size = tf.convert_to_tensor(step_size)
-  steps_remaining = steps
-  step = 0
-  while steps_remaining:
-    if steps_remaining>100:
-      run_steps = tf.constant(100)
-    else:
-      run_steps = tf.constant(steps_remaining)
-    steps_remaining -= run_steps
-    step += run_steps
+    # Convert from uint8 to the range expected by the model.
+    img = tf.keras.applications.inception_v3.preprocess_input(img)
+    img = tf.convert_to_tensor(img)
+    step_size = tf.convert_to_tensor(step_size)
+    steps_remaining = steps
+    step = 0
+    while steps_remaining:
+        if steps_remaining>100:
+            run_steps = tf.constant(100)
+        else:
+            run_steps = tf.constant(steps_remaining)
+        steps_remaining -= run_steps
+        step += run_steps
 
-    loss, img = deepdream(img, run_steps, tf.constant(step_size))
-    
-    display.clear_output(wait=True)
-    show(deprocess(img))
-    print ("Step {}, loss {}".format(step, loss))
+        loss, img = deepdream(img, run_steps, tf.constant(step_size))
+        
+        #display.clear_output(wait=True)
+        #show(deprocess(img))
+        print ("Step {}, loss {}".format(step, loss))
 
 
-  result = deprocess(img)
-  display.clear_output(wait=True)
-  show(result)
+    result = deprocess(img)
+    #display.clear_output(wait=True)
+    #show(result)
 
-  return result
+    return result
 
 dream_img = run_deep_dream_simple(img=original_img, 
-                                  steps=100, step_size=0.01)
+                                  steps=300, step_size=0.01)
+plt.imshow(dream_img, cmap='gray', vmin=0, vmax=1)
+plt.savefig('dream_img.png')
